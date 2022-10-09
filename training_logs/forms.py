a998ff358622867135fd317be2d4c8d34b7a00c0
gregflorkowski <gflorkowski4@gmail.com>
2022-10-09 14:37:26 -0400

    date format tip

--- training_logs/forms.py
+++ training_logs/forms.py
@@ -12,5 +12,5 @@ class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['hours','date_training_conducted','text']
-        labels = {'text':'','hours':'Hours','date_training_conducted':'Date of Training'}
+        labels = {'text':'','hours':'Hours','date_training_conducted':'Date of Training (yyyy-mm-dd)'}
         widgets = {'text':forms.Textarea(attrs={'cols':80})}
