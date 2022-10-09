c2d5a5524d94f06c959796430b4406bd6ff3546b
gregflorkowski <gflorkowski4@gmail.com>
2022-10-09 11:34:30 -0400

    static settings

--- training_log/settings.py
+++ training_log/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import django_on_heroku
 from decouple import config
 import dj_database_url
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +117,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
