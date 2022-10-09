37336c461aaaefed1eb19fd8c9cd82515f94f709
gregflorkowski <gflorkowski4@gmail.com>
2022-10-08 21:35:17 -0400

    rechange

--- training_log/settings.py
+++ training_log/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import django_on_heroku
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
